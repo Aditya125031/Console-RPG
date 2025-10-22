e8debe3c9264f7c0a4e6fab9d805dadc3783b9e5
Rautrao <rautraoambadas@gmail.com>
2025-10-22 14:11:27 +0530

    First commit added combat.hpp

--- include/combat.hpp
+++ include/combat.hpp
@@ -1,13 +1,9 @@
-#include"character.h"
-
+#include "character.h"
 #ifndef COMBAT_HPP
 #define COMBAT_HPP
 
 class Combat {
     protected:
     public:
-        //startFight(Character c,Enemy e);
-
 };
-
 #endif //COMBAT_HPP

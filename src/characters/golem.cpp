90b4829ddb8bfecd221f6f8cd755e9809ed567b7
Rautrao <rautraoambadas@gmail.com>
2025-11-04 22:52:32 +0530

    0 error file

--- src/characters/golem.cpp
+++ src/characters/golem.cpp
@@ -29,36 +29,11 @@ std::vector<Item> BoneGolem::getLoot(const Player& player) const
     }
     return allLoot;
 }
-void BoneGolem::specialAbility(Character& target) { 
-    
-    std::cout << get_name() << " uses 'Grave Harvest'!" << std::endl;
-    std::cout << "It launches jagged bones at you and pulls in dark energy!\n";
-
-    int damage_amount = 35;
-    std::cout << get_name() << " strikes you for " << damage_amount << " damage!\n";
-    target.take_damage(damage_amount);
-
-    int heal_amount = 70;
-    
-    if (this->health < this->maxHealth) 
-    {
-        this->health += heal_amount;
-        
-        if (this->health > this->maxHealth) {
-            this->health = this->maxHealth;
-        }
-        
-        std::cout << get_name() << " knits its bones back together, healing to " << this->health << " HP!\n";
-    } else {
-        std::cout << get_name() << " is already at full health!\n";
-    }
-}
 void BoneGolem::triggerDialogue() const {
     if (!dialogue.empty()) {
         std::cout << get_name() << " makes a sound: \"" << dialogue[rand() % dialogue.size()] << "\"\n";
     }
 }
-
 void BoneGolem::specialAbility(Character& target) { 
 
     std::cout << get_name() << " uses 'Grave Harvest'!" << std::endl;

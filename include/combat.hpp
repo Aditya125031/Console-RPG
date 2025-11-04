0770d586e244a9f271a146400c0fe9bc2a76a388
Shuvam Kayal <111375187+shuvam-kayal@users.noreply.github.com>
2025-11-04 15:49:43 +0530

    Revert "Revert "combat.cpp added""

--- include/combat.hpp
+++ include/combat.hpp
@@ -1,9 +1,14 @@
-#include "character.h"
+#include "player.h"
+#include "enemy.h"
 #ifndef COMBAT_HPP
 #define COMBAT_HPP
 
 class Combat {
-    protected:
+    private:
+        void start() const;
+        void end() const;
     public:
+        bool fight(Player&, Enemy&);
+        
 };
 #endif //COMBAT_HPP
